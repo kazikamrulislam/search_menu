e5d81d896236195a36368cadeeafb0609a55497f
kazikamrulislam <kamrul.lab@gmail.com>
2020-06-20 23:28:32 +0600

    aded js function for search bar

---
+++ Js/main.js
@@ -0,0 +1,16 @@
+function searchfun(){
+    let input, filter, ul, li, a, i;
+    input = document.getElementById("searchId");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("Menus");
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i< li.length; i++){
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+    }else {
+        li[i].style.display = "none";
+      }
+    }
+}
